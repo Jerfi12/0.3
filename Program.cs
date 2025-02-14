1a09e630416c8c786e0da2d01f4e0ed865453c21
Jerfi12 <dima.dima.stepanov79@mail.ru>
2025-02-14 10:29:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int imagesInRow = 3;
+            int totalImages = 52;
+            int fullRow = totalImages/imagesInRow;
+            Console.WriteLine($"Полных рядов: {fullRow}.");
+            int remainingImages = totalImages - (fullRow * imagesInRow);
+            Console.WriteLine($"Оставшихся изображений: {remainingImages}.");
+        }
+    }
+}
